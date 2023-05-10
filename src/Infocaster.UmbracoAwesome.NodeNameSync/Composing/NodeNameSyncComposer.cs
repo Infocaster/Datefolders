ffdaebf04e58009944e2beaba9a41049799fc312
Bram van den Bogaard <bram.van.den.bogaard@infocaster.net>
2023-05-10 13:28:49 +0000

    Merged PR 9060: v10 versie van nodenamesync gemaakt en test project toegevoegd
    v10 versie van nodenamesync gemaakt en test project toegevoegd

---
+++ src/Infocaster.UmbracoAwesome.NodeNameSync/Composing/NodeNameSyncComposer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Umbraco.Cms.Core.Composing;
+using Umbraco.Cms.Core.DependencyInjection;
+using Umbraco.Cms.Core.Manifest;
+
+namespace NodeNameSync.Composing
+{
+    public class NodeNameSyncComposer : IComposer
+    {
+        public void Compose(IUmbracoBuilder builder)
+        {
+            builder.ManifestFilters().Append<Manifest>();
+        }
+    }
+
+    internal class Manifest : IManifestFilter
+    {
+        public void Filter(List<PackageManifest> manifests)
+        {
+            manifests.Add(new PackageManifest
+            {
+                PackageName = "Infocaster.Umbraco.NodeNameSync",
+                Scripts = new[]
+             {
+               "/App_Plugins/Infocaster.NodeNameSync/Infocaster.NodeNameSync.controller.js"
+             },
+                Stylesheets = new[]
+             {
+               "/App_Plugins/Infocaster.NodeNameSync/Infocaster.NodeNameSync.css"
+             }
+            });
+        }
+    }
+}
